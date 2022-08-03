861bd2737b3ff9281d3d7476da479115ff31e56c
kun6685 <107320386+kun6685@users.noreply.github.com>
2022-08-04 01:36:53 +0900

    Add files via upload

---
+++ 1단계/1_7.js
@@ -0,0 +1,27 @@
+// 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+
+const fs = require('fs');
+let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+let newArr = [];
+for(let i=0; i<=input.length; ++i){
+    newArr.push(+input[i]);
+}
+
+const A = newArr[0];
+const B = newArr[1];
+
+console.log(A*B);
+
+/*const fs = require('fs');
+let input = fs.readFileSync("input.txt").toString().trim().split(" ");
+
+let newArr = [];
+for(let i=0; i<=input.length; ++i){
+    newArr.push(+input[i]);
+}
+
+const A = newArr[0];
+const B = newArr[1];
+
+console.log(A*B);*/
