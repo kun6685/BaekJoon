c1cfad77312b5c05902e2aeca2843d948ca1fb18
kun6685 <107320386+kun6685@users.noreply.github.com>
2022-08-07 16:25:07 +0900

    Add files via upload

--- 1단계/1_10.js
+++ 1단계/1_10.js
@@ -2,10 +2,13 @@
 // 준하는 놀람을 ??!로 표현한다. 준하가 가입하려고 하는 사이트에 이미 존재하는 아이디가 주어졌을 때, 놀람을 표현하는 프로그램을 작성하시오.
 
 const fs = require('fs');
-let input = fs.readFileSync("/dev/stdin").toString();
-if(input == 'joonas'){
-console.log('joonas??!');
-}
+let input = fs.readFileSync('/dev/stdin',{encoding : 'utf-8'}).trim();
 
+console.log(input + '??!');
 
-// 진행중
+/*
+const fs = require('fs');
+let input = fs.readFileSync('input.txt',{encoding : 'utf-8'}).trim();
+
+console.log(input + '??!');
+*/
