ec98fffed24bc9e907bbcc461dcf87521efd9ccc
kun6685 <107320386+kun6685@users.noreply.github.com>
2022-08-12 11:42:33 +0900

    Add files via upload

---
+++ 1단계/1_15.js
@@ -0,0 +1,16 @@
+// 아래 예제와 같이 새싹을 출력하시오.
+
+//           ,r'"7
+// r`-_   ,'  ,/
+// \. ". L_r'
+//   `~\/
+//      |
+//      |
+
+console.log('         ,r\'"7');
+console.log('r`-_   ,\'  ,/');
+console.log(' \\. ". L_r\'');
+console.log('   `~\\/');
+console.log('      |');
+console.log('      |');
+
