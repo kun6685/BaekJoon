a5c13f47b290876de903a07a4837096622ff8db9
kun6685 <107320386+kun6685@users.noreply.github.com>
2022-08-21 01:26:34 +0900

    Add files via upload

--- 2단계/2_4.js
+++ 2단계/2_4.js
@@ -1,19 +1,6 @@
 // 점의 좌표를 입력받아 그 점이 어느 사분면에 속하는지 알아내는 프로그램을 작성하시오.
-// 진행중
 
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    input = line.split('/n').map((el) => parseInt(el));
-  })
-  .on('close', function () {
-    
+function solution(input1, input2) {
 
     if((-1000<=input[0] && input[0] <= 1000 && input[0]!=0) && (-1000<=input[1] && input[1] <= 1000 && input[1]!=0)){
 
@@ -35,5 +22,22 @@ rl.on('line', function (line) {
             break;
         }
         }
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+let input = [];
+rl.on("line", function(line) {
+    input.push(line)
+}).on("close", function() {
+    let list1 = input[0].split(' ').map((el) => parseInt(el));
+    let list2 = input[1].split(' ').map((el) => parseInt(el));
+    solution(list1, list2);
     process.exit();
-  });
+});
+
+
+
